5db8b5a9b58e0a20f2b288a0ddba8a9020ee9311
Kachanka <kachanka01@gmail.com>
2025-02-08 20:27:43 +0500

    diplom

---
+++ config.py
@@ -0,0 +1,9 @@
+BASE_URL = "https://www.kinopoisk.ru/"
+REGISTER_URL = "https://passport.yandex.ru/"
+USERNAME = "kachanka01@gmail.com"
+PASSWORD = "Aloxa100"
+API_BASE_URL = "https://api.kinopoisk.dev/v1.4"
+API_TOKEN = "uPpbHnncoO9lFMusYnc7pN-_Yp8AUcWD16-4nTbPgQ4"
+HEADERS = {
+    "X-API-KEY": API_TOKEN
+}
